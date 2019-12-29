1051742ece30770ee6d710c9dc0ef295e083729b
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-29 21:42:05 +0900

    Initial Commit

---
+++ MemoryReactivationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryReactivationApp
+//
+//  Created by 宏輝 on 29/12/2019.
+//  Copyright © 2019 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
